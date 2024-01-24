ded755a085e6448396d372c2df9ebc6ef6b95273
nammar1999 <nammar@missouriwestern.edu>
2024-01-24 10:45:04 -0600

    Updated to add buttons and test question.

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,21 +14,32 @@ import com.bignerdranch.android.geoquiz.ui.theme.GeoQuizTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GeoQuizTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+                    val trueButton: Button = findViewById(R.id.true_button)
+                    val falseButton: Button = findViewById(R.id.false_button)
+
+                    trueButton.setOnClickListener { _: View ->
+                        Toast.makeText(
+                            this,
+                            R.string.correct_toast,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    falseButton.setOnClickListener { _: View ->
+                        Toast.makeText(
+                            this,
+                            R.string.incorrect_toast,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
-        }
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
+            text = "There goes $name!",
             modifier = modifier
     )
 }
@@ -38,6 +48,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GeoQuizTheme {
-        Greeting("Android")
+        Greeting("Walter Bicklein")
     }
 }
